a2e81fec29e678990e42119804dc5942fdf5dcf1
Rifqi Charisma <rifqibarker@gmail.com>
2021-11-08 10:34:04 +0700

    update modul dpw

---
+++ 01. PRAKTIKUM/Pertemuan3/script.js
@@ -0,0 +1,9 @@
+function tampil() {
+    alert("Hallo")
+}
+
+let warna = document.getElementById('warna')
+
+warna.addEventListener('change', (event) =>{
+    document.body.style.backgroundColor = warna.value
+})
